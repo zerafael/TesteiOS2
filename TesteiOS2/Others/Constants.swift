aa6114d264814cec8200e61d67f19ea095b16de7
José Rafael <zerafael1@gmail.com>
2019-12-30 03:48:23 -0400

    Add constants file. Set button colors

---
+++ TesteiOS2/Others/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  TesteiOS2
+//
+//  Created by José Rafael Ferraz Pacheco on 30/12/19.
+//  Copyright © 2019 José Rafael Ferraz Pacheco. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct colors {
+        static let button = "#DA0101"
+        static let buttonPressed = "#EB7676"
+    }
+}
